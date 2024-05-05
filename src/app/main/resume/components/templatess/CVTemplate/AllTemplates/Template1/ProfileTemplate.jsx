b27ce9807b3a1a323739edb9d986b4286661cb97
Thanak Mech <thanakmech@gmail.com>
2024-05-05 22:15:39 +0700

    added profile form to edit resume folder

--- src/app/main/resume/components/templatess/CVTemplate/AllTemplates/Template1/ProfileTemplate.jsx
+++ src/app/main/resume/components/templatess/CVTemplate/AllTemplates/Template1/ProfileTemplate.jsx
@@ -4,7 +4,7 @@ import parse from "html-react-parser";
 function ProfileTemplate({resumeData}) {
   return (
     <div className='prose px-5 mb-5 text-black max-w-full text-justify text-sm'>
-        {parse(resumeData.data.profile.summary)}
+        {resumeData?.data?.profile?.summary && parse(resumeData?.data?.profile?.summary)}
     </div>
   )
 }
