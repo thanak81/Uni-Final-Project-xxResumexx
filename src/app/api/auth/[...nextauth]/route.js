166a49a904b7ccad8f389db5315d5593ccc52684
Thanak Mech <thanakmech@gmail.com>
2024-05-14 20:01:45 +0700

    deploy error

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -10,10 +10,6 @@ export const authOptions = {
     adapter: PrismaAdapter(prisma),
     
     providers:[
-      GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET
-    }),
         CredentialsProvider({
             name : "Credentials",
             credentials: {
